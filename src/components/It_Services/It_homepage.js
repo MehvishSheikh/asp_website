ddba86d5597bed2ed66b1ed141966512eed49665
Sahaji Chaurasia <130205533+sahaji-2003@users.noreply.github.com>
2023-11-10 20:08:15 +0530

    it-service

---
+++ src/components/It_Services/It_homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FooterComponent from '../FooterComponent'
+import HeadComponent from '../HeadComponent'
+
+const It_homepage = () => {
+  return (
+    <div className='itservices'>
+        <HeadComponent />
+       <FooterComponent />
+    </div>
+  )
+}
+
+export default It_homepage
